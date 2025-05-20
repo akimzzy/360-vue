9725fa3f78eb473d4121bed1626587fb1bc0ccc1
Adeyemi Akeem <a.akeem@yahoo.com>
2025-05-20 11:52:10 +0100

    Scaffolded with the first three component

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import FlightDetails from './components/ui/FlightDetails.vue'
+import PumpFill from './components/ui/PumpFill.vue'
+import TranslateLanguage from './components/ui/TranslateLanguage.vue'
+</script>
+
+<template>
+  <div class="flex h-full gap-8 flex-wrap">
+    <FlightDetails />
+    <PumpFill />
+    <TranslateLanguage />
+  </div>
+</template>
+
+<style scoped></style>
